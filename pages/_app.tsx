e563f80c69c1a62d5082e0e806478de15936d501
David M. Fajardo <davidmf99@gmail.com>
2021-10-26 21:27:48 +0200

    menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/global.scss'
+import 'tailwindcss/tailwind.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,49 @@
-import React, { useEffect } from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react'
+import Welcome from '../components/Welcome'
 import { load, save } from '../lib/localstorage'
 
-const Index = () => {
+export default function Index() {
 
 	const state = {
 		hp: 12
 	}
 
 	const start = () => {
-		save({
-			hp: 15
-		})
-		const prueba = load()
-		console.log(prueba)
+		console.log("hofsadhf")
 	}
+	
+	const [menuVisibility, setMenuVisibility] = useState(false)
+	const [firstRender, setFirstRender] = useState(true)
+	const [playing, setPlaying] = useState(false)
 
-	const sprite = (sprite_index) => {
-		const sprites = {
-			0: 'grass',
-			1: 'sea1',
-			2: 'sea2',
-			5: 'border_top',
-			6: 'border_right',
-		}
-		return sprites[sprite_index] 
-	}
+	const title_class = ' text-white font-bold tracking-wider font-2xl p-3 rounded-xl bg-gradient-to-br '
+	const coming = ' animate__animated animate__fadeIn animate__fast'
+	const leaving = ' animate__animated animate__fadeOut animate__fast '
 
-	const l0 = [0,0,0,0,0,0,0,0,0,0,6,1,1,1,2,1] 
-	const l1 = [0,0,0,0,0,0,0,0,0,0,0,5,1,2,1,2] 
-	const l2 = [0,0,0,0,0,0,0,0,0,0,0,0,5,1,2,1] 
-	const l3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,1] 
-	const l4 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5] 
-	const l5 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-	const l6 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-	const l7 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-	const l8 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-	const l9 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
-	const world = [l0, l1, l2, l3, l4, l5, l6, l7, l8, l9]
+	const animation = firstRender ? 'opacity-0' : menuVisibility ? coming : leaving
+
+	// if (loading) return <Loader />
 
 	return (
-		<div>
-			{ world.map((line, l_index) => {
-				return (
-					line.map((x, x_index) => {
-						const type = sprite(x)
-						console.log(line.length, x_index)
-						return (
-							<div
-								key={l_index + "_" + x_index} 
-								className={"sprite " + type}
-								style={{clear: x_index==0 ? 'both' : 'none'}}
-							></div>
-						)
-					}) 
-				)
-			})}
-			
-
-			<br/>
-			<button onClick={start}>START</button>
-		</div>
+		<>
+		{ !playing &&
+			<Welcome
+				setFirstRender={setFirstRender}
+				setMenuVisibility={setMenuVisibility}
+				menuVisibility={menuVisibility}
+				title_class={title_class}
+				animation={animation}
+				start={start}
+			/>
+		}
+		{ playing &&
+			<div className="backgroundPlaying h-screen">
+				<div className="bg-gray-800 h-screen opacity-90">
+					<button onClick={start}>START</button>
+				</div>
+			</div>
+		}
+		</>
 	)
 }
-
-export default Index
