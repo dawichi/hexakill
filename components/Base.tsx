e09a533b15692ed8e50ca102de8ae0307f40fad2
David M. Fajardo <davidmf99@gmail.com>
2021-10-26 22:57:37 +0200

    Update Base.tsx

--- components/Base.tsx
+++ components/Base.tsx
@@ -3,15 +3,20 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 
 const Base = () => {
 
-	const [health, setHealth] = useState(11)
+	const [life, setLife] = useState(100)
 
 	const getDamage = (damage: number) => {
-		setHealth(health - damage)
+		if (damage > life) return
+		setLife(life - damage)
+	}
+	const getHealth = (health: number) => {
+		if (health + life > 100) return
+		setLife(life + health)
 	}
 
 	const colorHP = (hp: number) => {
 		if (hp < 10) return 'danger'
-		if (hp >= 10 && hp < 21) return 'warning'
+		if (hp >= 10 && hp < 31) return 'warning'
 		return 'success'
 	}
 
@@ -26,22 +31,23 @@ const Base = () => {
 									<h2 className="text-white text-center font-bold tracking-wider text-2xl p-3">Stats</h2>
 									<hr style={{backgroundColor: 'white', width: '80%', margin: 'auto'}}/>
 									<ul className="text-white p-5">
-										<li>HP: 1204123</li>
-										<li>HP: 1204123</li>
-										<li>HP: 1204123</li>
-										<li>HP: 1204123</li>
-										<li>HP: 1204123</li>
+										<li>HP: 1000</li>
+										<li>HP: 1000</li>
+										<li>HP: 1000</li>
+										<li>HP: 1000</li>
+										<li>HP: 1000</li>
 									</ul>
 								</div>
 							</div>
 						</div>
 						<div className="col-6 h-100">
 							<div className="border-black rounded bg-black h-100">
-								<div className="p-5">
-									<ProgressBar striped animated variant={colorHP(health)} key={1} now={health} />
+								<div className="p-5 text-white">
+									<p>HP: {life}%</p>
+									<ProgressBar striped animated variant={colorHP(life)} key={1} now={life} />
 								</div>
-								<button className="border p-3 m-4 bg-white" onClick={() => setHealth(health - 5)}>DAMAGE</button>
-								<button className="border p-3 m-4 bg-white" onClick={() => setHealth(health + 5)}>CURASIOON</button>
+								<button className="border p-3 m-4 bg-white" onClick={() => getDamage(5)}>DAMAGE</button>
+								<button className="border p-3 m-4 bg-white" onClick={() => getHealth(5)}>CURASION</button>
 							</div>
 						</div>
 						<div className="col-3 h-100">
@@ -49,9 +55,6 @@ const Base = () => {
 								<div>
 									<h2 className="text-white text-center font-bold tracking-wider text-2xl p-3">Items</h2>
 									<hr style={{backgroundColor: 'white', width: '80%', margin: 'auto'}}/>
-									<div className="progress">
-										<div className="progress-bar" role="progressbar" style={{width: '25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
-									</div>
 								</div>
 							</div>
 						</div>
