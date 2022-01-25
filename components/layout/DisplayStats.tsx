62d2a3fbebdd82e4c0f34ffdbae83faec3e385f3
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-01-25 20:49:25 +0100

    Player Stats

--- components/layout/DisplayStats.tsx
+++ components/layout/DisplayStats.tsx
@@ -1,14 +1,52 @@
 const DisplayStats = ({ player }) => {
     return (
-        <div>
-            <p>health: {player.health}</p>
-            <p>ability_power: {player.ability_power}</p>
-            <p>attack_damage: {player.attack_damage}</p>
-            <p>critical_hit: {player.critical_hit}</p>
-            <p>magic_resist: {player.magic_resist}</p>
-            <p>armor: {player.armor}</p>
-            <p>movement_speed: {player.movement_speed}</p>
-            <p>luck: {player.luck}</p>
+        <div> 
+            
+            <p>
+                <span className='rounded px-1 text-white bg-green-600'>health: {player.health}
+                <i className='px-1 bi bi bi-heart-fill text-black'></i>
+                </span>
+            </p>
+
+            <p>
+            <span className='rounded px-1 text-white bg-blue-600'>ability_power: {player.ability_power}</span>
+            <i className='px-1 bi bi bi-magic'></i>
+                </p>
+
+            <p>
+            <span className='rounded px-1 text-white bg-red-600'>attack_damage: {player.attack_damage}</span>
+            <i className='px-1 bi bi-hammer'></i>
+            </p>
+
+            <p>
+            <span className='rounded px-1 text-white bg-yellow-600'>crit_chance: {player.critical_hit}
+            <i className='px-1 bi bi bi bi-bullseye'></i>
+            </span>
+            </p>
+
+            <p>
+            <span className='rounded px-1 text-white bg-purple-600'>magic_resist: {player.magic_resist}
+            <i className='px-1 bi bi bi-stars'></i>
+            </span>
+            </p>
+
+            <p>
+            <span className='rounded px-1 text-white bg-amber-900'>armor: {player.armor}
+            <i className='px-1 bi bi-shield-shaded'></i>
+            </span>
+            </p>
+
+            <p>
+            <span className='rounded px-1 text-white bg-cyan-400'>movement_speed: {player.movement_speed}
+            <i className='px-1 bi bi-wind'></i>
+            </span>
+            </p>
+
+            <p>
+            <span className='rounded px-1 text-white bg-lime-400'>luck: {player.luck}</span>
+            <i className='px-1 bi bi-flower2'></i>
+            </p>
+        
         </div>
     )
 }
