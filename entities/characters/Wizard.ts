4e8f34086196a97673bb4e636288a1eda23db76f
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-02-05 00:45:01 +0100

    Stat changes

--- entities/characters/Wizard.ts
+++ entities/characters/Wizard.ts
@@ -13,8 +13,8 @@ class Wizard extends Character {
         super(level, `${name} (Wizard)`)
         this.image = 'wizard'
         this.size = 'w-36 h-56'
-        this.ad = level * base.health * 1.5
-        this.ad = level * base.ap * 2.5
+        this.health = level * base.health * 1.5
+        this.ap = level * base.ap * 2.5
         this.ad = level * base.ad * 1.5
     }
 
